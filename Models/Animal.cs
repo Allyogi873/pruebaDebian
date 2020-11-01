0692e149b9abf87551a74b6a606545f2843f295a
Allyogi <allan1quezada@gmail.com>
2020-10-31 20:18:08 -0600

    Initial commit

---
+++ Models/Animal.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace pruebadebian.Models
+{
+    public partial class Animal
+    {
+        public int CodAnimal { get; set; }
+        public string Nombre { get; set; }
+        public string Especie { get; set; }
+        public string Habitat { get; set; }
+        public string Alimentacion { get; set; }
+    }
+}
---
+++ Models/prueba1Context.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace pruebadebian.Models
+{
+    public partial class prueba1Context : DbContext
+    {
+        public prueba1Context()
+        {
+        }
+
+        public prueba1Context(DbContextOptions<prueba1Context> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Animal> Animal { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseMySQL("uid=root;password=A50568613;server=127.0.0.1;port=3306;database=prueba1;sshHostName=192.168.0.7;sshUserName=allan;sshPassword=A50568613");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Animal>(entity =>
+            {
+                entity.HasKey(e => e.CodAnimal)
+                    .HasName("PRIMARY");
+
+                entity.ToTable("animal");
+
+                entity.Property(e => e.CodAnimal).HasColumnName("cod_animal");
+
+                entity.Property(e => e.Alimentacion)
+                    .HasColumnName("alimentacion")
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Especie)
+                    .HasColumnName("especie")
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Habitat)
+                    .HasColumnName("habitat")
+                    .HasMaxLength(45);
+
+                entity.Property(e => e.Nombre)
+                    .HasColumnName("nombre")
+                    .HasMaxLength(45);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
